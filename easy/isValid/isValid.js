65e5d6cbb67a02d9b9a894d5331303983d15e37f
Irvin Solano <irvin.solano.ny@gmail.com>
2022-11-26 13:30:33 -0500

    cleaned up comments

---
+++ easy/isValid/isValid.js
@@ -0,0 +1,32 @@
+// https://leetcode.com/problems/valid-parentheses/description/
+
+const isValid = function (s) {
+  if (s.length === 1) {
+    return false;
+  } else {
+    for (let i = 0; i < s.length; i += 2) {
+      switch (s[i]) {
+        case '[':
+          if (s[i + 1] !== ']') {
+            return false;
+          }
+          break;
+        case '{':
+          if (s[i + 1] !== '}') {
+            return false;
+          }
+          break;
+        case '(':
+          if (s[i + 1] !== ')') {
+            return false;
+          }
+          break;
+        default:
+          return false;
+      }
+    }
+    return true;
+  }
+};
+
+export default isValid;
