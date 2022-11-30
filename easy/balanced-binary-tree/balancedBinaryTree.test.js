e61e6c4cc29de271bad12e7ba626e6d90a72016c
Irvin Solano <irvin.solano.ny@gmail.com>
2022-11-29 19:06:34 -0500

    added tests

--- easy/balanced-binary-tree/balancedBinaryTree.test.js
+++ easy/balanced-binary-tree/balancedBinaryTree.test.js
@@ -1,5 +1,5 @@
 import isBalanced from './balancedBinaryTree.js';
-import makeATree from '../../utils.js';
+import makeATree from '../../utils/utils.js';
 const trees = [[3, 9, 20, null, null, 15, 7], [1, 2, 2, 3, 3, null, null, 4, 4], []];
 const testCases = ['when a tree is balanced, the function outputs TRUE', 'when a tree is NOT balanced, the function outputs FALSE', 'empty set'];
 const testOutputs = [true, false, true];
