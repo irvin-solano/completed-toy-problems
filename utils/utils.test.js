4ecb6d3b2c6c79bdaccc4a38a2f8176f7b623873
Irvin Solano <irvin.solano.ny@gmail.com>
2022-11-29 19:08:31 -0500

    added tests for  makeATreeto verify it is properly creating unbalanced trees, and resolved failing tests

---
+++ utils/utils.test.js
@@ -0,0 +1,42 @@
+import makeATree from './utils.js';
+import balancedBinaryTree from '../easy/balanced-binary-tree/balancedBinaryTree.js';
+
+describe('makeATree makes a binary tree when given an array', () => {
+  it('[] results in a node with a default value of 0', () => {
+    const tree = makeATree([]);
+    expect(tree.val === 0).toBe(true);
+  });
+  it('[] results in a node with no branches', () => {
+    const tree = makeATree([]);
+    expect(tree.right === null && tree.left === null).toBe(true);
+  });
+  it('[1] results in one node, with a value of 1', () => {
+    const tree = makeATree([1]);
+    expect(tree.val === 1 && tree.right === null && tree.left === null).toBe(true);
+  });
+  it('[1] results in node with no other branches', () => {
+    const tree = makeATree([1]);
+    expect(tree.val === 1 && tree.right === null && tree.left === null).toBe(true);
+  });
+  it('[1] results in one node, with a value of 1', () => {
+    const tree = makeATree([1]);
+    expect(tree.val === 1 && tree.right === null && tree.left === null).toBe(true);
+  });
+  it('[1] results in node with no other branches', () => {
+    const tree = makeATree([1]);
+    expect(tree.val === 1 && tree.right === null && tree.left === null).toBe(true);
+  });
+  it('[1,2] results in a tree, with a left branch with a value of 2 and no other branches', () => {
+    const tree = makeATree([1, 2]);
+    expect(tree.left.val === 2 && tree.left.left === null && tree.left.right === null).toBe(true);
+  });
+  it('[1,2] results in tree without a right branch', () => {
+    const tree = makeATree([1, 2]);
+    console.log(tree);
+    expect(tree.right === null).toBe(true);
+  });
+  it('[1,2,3,4,5,null,null,null,null,10,11] results in tree without a right branch', () => {
+    const tree = makeATree([1, 2, 3, 4, 5, null, null, null, null, 10, 11]);
+    expect(balancedBinaryTree(tree)).toBe(false);
+  });
+});
